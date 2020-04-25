57edf0b426ece4eb8e1c4d42f3bd7dcd1dbc896d
Pierluigi Cifani <pcifani@theleftbit.com>
2020-04-25 17:12:34 +0200

    Bump dependencies

--- Package.swift
+++ Package.swift
@@ -8,9 +8,9 @@ let package = Package(
     ],
     dependencies: [
         // 💧 A server-side Swift web framework.
-        .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0-rc"),
-        .package(url: "https://github.com/vapor/fluent.git", from: "4.0.0-rc"),
-        .package(url: "https://github.com/vapor/fluent-postgres-driver.git", from: "2.0.0-rc")
+        .package(url: "https://github.com/vapor/vapor.git", from: "4.4.0"),
+        .package(url: "https://github.com/vapor/fluent.git", from: "4.0.0-rc.2.1"),
+        .package(url: "https://github.com/vapor/fluent-postgres-driver.git", from: "2.0.0-rc.1.1")
     ],
     targets: [
         .target(name: "App", dependencies: [
